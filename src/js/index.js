dfea834b296ce6c1e6562562d4eeab3a356a11c0
Peter Vliet <plvliet@us.ibm.com>
2016-02-24 11:01:29 -0800

    add action creators to format state changes

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
+import {setState} from './action_creators';
 import {AppContainer} from './components/App';
 
+let state = {
+  name: 'Peter'
+}
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    name: 'Peter'
-  }
-});
+
+store.dispatch(setState(state));
 
 ReactDOM.render(
   <Provider store={store}>
