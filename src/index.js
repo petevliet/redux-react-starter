58dd2fdfd3461dfa4c7e5464c5d7048da34c6c53
Peter Vliet <plvliet@us.ibm.com>
2016-02-23 17:13:26 -0800

    render app component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('I am alive!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/components/App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
---
+++ test/components/App_spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import {expect} from 'chai';
+import App from '../../src/js/components/App';
+
+describe('App', () => {
+
+  it('renders on the document', () => {
+      const appComponent = TestUtils.renderIntoDocument(<App />);
+      const componentDiv = TestUtils.findRenderedDOMComponentWithClass(
+        appComponent, 'main'
+      );
+      
+      expect(componentDiv.textContent).to.equal('hello');
+  });
+});
