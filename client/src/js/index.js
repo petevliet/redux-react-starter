69ae8a2867aa55d196848a0ab76b279203084ef8
Peter Vliet <plvliet@us.ibm.com>
2016-02-24 16:08:28 -0800

    put client side files in dir

---
+++ client/src/js/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import {AppContainer} from './components/App';
+
+let state = {
+  name: 'buddy'
+}
+const store = createStore(reducer);
+
+store.dispatch(setState(state));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('app')
+);
